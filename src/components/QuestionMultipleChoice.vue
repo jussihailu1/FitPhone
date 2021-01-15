55064c9ad5ba718101129d74bc04e985166a0384
Jussi <jussihailu1@gmail.com>
2021-01-15 14:47:12 +0100

    Styling

--- src/components/QuestionMultipleChoice.vue
+++ src/components/QuestionMultipleChoice.vue
@@ -1,26 +1,31 @@
 <template>
-  <div class="bg-gray-800 m-1 p-1 rounded-3xl">
+  <div class="bg-green-200 m-1 p-1 rounded-3xl">
     <div class="text-center">
       <label @click="test" class="">
         {{ question.question }}
       </label>
     </div>
-
     <br />
-    <div v-for="a in question.answers" :key="a.id">
-      <label :class="$store.state.day ? 'container day' : 'container night'" @click="setAnswer(a)" :for="a.id">
-        {{ a.value }}
-        <input
-          class="checkbox"
-          type="radio"
-          :id="a.id"
-          :name="question.id"
-          :value="a"
-          v-model="answer"
-        />
-        <span class="checkmark"></span>
-      </label>
-      <br />
+    <div class="answers">
+      <div class="answer" v-for="a in question.answers" :key="a.id">
+        <label
+          :class="$store.state.day ? 'container day' : 'container night'"
+          @click="setAnswer(a)"
+          :for="a.id"
+        >
+          {{ a.value }}
+          <input
+            class="checkbox"
+            type="radio"
+            :id="a.id"
+            :name="question.id"
+            :value="a"
+            v-model="answer"
+          />
+          <span class="checkmark"></span>
+        </label>
+        <br />
+      </div>
     </div>
     <br />
   </div>
@@ -52,9 +57,8 @@ export default {
 
 <style scoped>
 .container {
-  display: block;
   position: relative;
-  padding-left: 2.5rem;
+  padding-left: 2rem;
 }
 
 /* Hide the browser's default checkbox */
@@ -69,7 +73,7 @@ export default {
 /* Create a custom checkbox */
 .checkmark {
   position: absolute;
-  top: 0.5rem;
+  top: 0.18rem;
   left: 0.5rem;
   height: 1rem;
   width: 1rem;
@@ -79,11 +83,21 @@ export default {
 
 /* When the checkbox is checked, add a blue background */
 .container.day input:checked ~ .checkmark {
-  background-color:#ff9e2e;
+  background-color: #ff9e2e;
 }
 
 /* When the checkbox is checked, add a gold background */
 .container.night input:checked ~ .checkmark {
-  background-color:#416cbb;
+  background-color: #416cbb;
+}
+
+.answers {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.answer {
+  width: 50%;
+  margin-bottom: 1vh;
 }
 </style>
